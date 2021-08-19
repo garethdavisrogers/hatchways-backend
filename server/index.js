f559b78883fc0680b46bed355ad4d4232dd45da0
Gareth Rogers <garethdavisrogers@gmail.com>
2021-08-19 13:23:27 -0500

    axios promise finally usable

--- server/index.js
+++ server/index.js
@@ -1,11 +1,56 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static("./client/public"));
+const axios = require("axios");
 
-app.get('/', (req, res)=>{
-  res.send('hey')
-})
+function fetchInit() {
+  return axios
+    .get("https://api.hatchways.io/assessment/blog/posts", {
+      params: { tag: "tech" },
+    })
+    .then((res) => res.data.posts)
+    .catch(() => console.log("fetch failed"));
+}
+
+const searchBy = async (t, sb, o) => {
+  debugger;
+  const data = await fetchInit();
+  let result = [];
+  t = t.split(",");
+  console.log(t);
+  data.forEach((item) => {
+    if (item.tags.includes(t)) {
+      result.push(item);
+    }
+  });
+  return result;
+};
+app.get("/api/ping", (req, res) => {
+  try {
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Server Error");
+  }
+});
+
+app.get("/api/posts", async (req, res) => {
+  try {
+    const { tags, sortBy, direction } = req.query;
+    let sb = sortBy ? sortBy : "id";
+    let dir = direction ? direction : "asc";
+    if (tags) {
+      const search = searchBy(tags, sb, dir);
+      console.log(search);
+      res.status(200).send(search);
+    } else {
+      return res.status(400).json({ error: "Tags parameter is required" });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Server Error");
+  }
+});
 app.listen(port, () => {
   console.log("node is listening");
 });
